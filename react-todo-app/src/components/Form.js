0f3151eb014f024620b377e3420ccb4579fa065e
jaehan.kim <mju6013@naver.com>
2022-09-20 00:44:33 +0900

    tailwind css 적용

--- react-todo-app/src/components/Form.js
+++ react-todo-app/src/components/Form.js
@@ -8,10 +8,11 @@ export default function Form({value, setValue, handleSubmit}) {
 
   return (
     <div>
-      <form style= {{display:'flex'}} onSubmit={handleSubmit}>
+      <form style= {{display:'flex'}} onSubmit={handleSubmit} className="flex pt-2 ">
           <input 
             type="text" 
             name="value" 
+            className='w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow'
             style={{flex:'10', padding: '5px'}} 
             placeholder ="해야 할 일을 입력하세요." 
             value={value}
@@ -20,7 +21,7 @@ export default function Form({value, setValue, handleSubmit}) {
           <input 
             type="submit"
             value="입력"
-            className="btn"
+            className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-200"
             style={{flex:'1'}}
             />
         </form>
