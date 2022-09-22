1557c144c3490478285e3c85972f674455ab1638
kjhqwaszx <kjhqwaszx@naver.com>
2022-09-22 16:00:55 +0900

    netflix nav bar 구현

---
+++ react-netflix/src/components/Nav.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
+import netflix_logo from '../assets/img/netflix_logo.svg';
+import react_logo from '../assets/img/react_logo.png';
+
+const Nav = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    //scroll 이벤트가 발생했을때 실행
+    window.addEventListener('scroll', () => {
+      console.log(window.scrollY);
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener('scroll', () => {});
+    };
+  }, []); // dependency 변수설정
+
+  return (
+    <nav className={`nav ${show && 'nav__black'}`}>
+      <img
+        alt="Netflix logo"
+        src={netflix_logo}
+        className="nav__logo"
+        onClick={() => window.location.reload()}
+      />
+      <img alt="User logged" src={react_logo} className="nav__avatar" />
+    </nav>
+  );
+};
+
+export default Nav;
