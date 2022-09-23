60d2d184642441e233f3a93b9f0753a22cf3e5d6
jaehan <mju6013@naver.com>
2022-09-23 20:16:49 +0900

    movie contents 구현

--- react-netflix/src/App.js
+++ react-netflix/src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import Nav from './components/Nav';
 import Banner from './components/Banner';
+import requests from './api/requests';
+import Row from './components/Row';
 
 const App = () => {
   return (
     <div className="App">
       <Nav></Nav>
       <Banner></Banner>
+
+      <Row
+        title="Netflix Original"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      ></Row>
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending}></Row>
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated}></Row>
+      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies}></Row>
+      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies}></Row>
     </div>
   );
 };
