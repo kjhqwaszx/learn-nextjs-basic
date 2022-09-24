230024abc399204fc66762102b50f5979fa36d14
jaehan <mju6013@naver.com>
2022-09-24 18:02:02 +0900

    modal 구현

--- react-netflix/src/components/Row.js
+++ react-netflix/src/components/Row.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from '../api/axios';
 import './Row.css';
+import MovieModal from './MovieModal';
 
 const Row = ({ isLargeRow, title, id, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState({});
 
   useEffect(() => {
     fetchMovieData();
@@ -12,6 +15,11 @@ const Row = ({ isLargeRow, title, id, fetchUrl }) => {
   const fetchMovieData = async () => {
     const request = await axios.get(fetchUrl);
     setMovies(request.data.results);
+  };
+
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setSelectedMovie(movie);
   };
 
   return (
@@ -35,6 +43,7 @@ const Row = ({ isLargeRow, title, id, fetchUrl }) => {
                 isLargeRow ? movie.poster_path : movie.backdrop_path
               }`}
               alt={movie.name}
+              onClick={() => handleClick(movie)}
             />
           ))}
         </div>
@@ -48,6 +57,7 @@ const Row = ({ isLargeRow, title, id, fetchUrl }) => {
           <span className="arrow">{'>'}</span>
         </div>
       </div>
+      {modalOpen && <MovieModal {...selectedMovie} setModalOpen={setModalOpen}></MovieModal>}
     </section>
   );
 };
