221f1ff21df17ad6f4aa8019661dfba99797d6cb
jaehan <mju6013@naver.com>
2022-09-27 16:56:53 +0900

    console.log 제거

--- react-netflix/src/hooks/useOnClickOutside.js
+++ react-netflix/src/hooks/useOnClickOutside.js
@@ -4,8 +4,6 @@ const UseOnClickOutside = (ref, handler) => {
   useEffect(() => {
     //eventListener 를 설정해 마우스 클릭이 발생 할경우 callback함수 실행
     const listener = (event) => {
-      console.log('ref', ref.current);
-      console.log('target', event.target);
       if (!ref.current || ref.current.contains(event.target)) {
         return;
       }
