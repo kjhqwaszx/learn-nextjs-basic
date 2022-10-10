903ca299374614338a996364dd9346520d654027
jaehan.kim <mju6013@naver.com>
2022-10-10 15:49:09 +0900

    tailwind css 적용

--- nextjs-commerce/pages/index.js
+++ nextjs-commerce/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import { useEffect, useRef, useState } from 'react';
+import { css } from '@emotion/react';
+import Button from '../components/Button';
 
 export default function Home() {
   const inputRef = useRef(null);
@@ -46,8 +48,24 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <input ref={inputRef} type="text" placeholder="name" />
-        <button onClick={handleClick}> Add Jacket</button>
+        <input
+          ref={inputRef}
+          className="placeholder:italic placeholder:text-slate-400 block bg-white w-96 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          type="text"
+          placeholder="name"
+        />
+        <button
+          css={css`
+            background-color: hotpink;
+            padding: 16px;
+            border-radius: 8px;
+          `}
+          onClick={handleClick}
+        >
+          {' '}
+          Add Jacket
+        </button>
+        <Button onClick={handleClick}>Add Jacket2 </Button>
 
         <div>
           <p>Product List</p>
