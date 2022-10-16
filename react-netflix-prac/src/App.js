3c09550a4ec5696173b28b0baf9f59cebb71f942
jaehan.kim <mju6013@naver.com>
2022-10-16 18:43:20 +0900

    Row 구현

--- react-netflix-prac/src/App.js
+++ react-netflix-prac/src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 import Nav from './components/Nav';
 import Banner from './components/Banner';
+import Row from './components/Row';
+import requests from './api/requests';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
       <Banner></Banner>
+      <Row
+        title="Netflix Original"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
     </div>
   );
 }
