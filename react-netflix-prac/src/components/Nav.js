ab04fca0a9226db2215863b5fdfabb61757b6f9b
jaehan.kim <mju6013@naver.com>
2022-10-16 16:35:53 +0900

    프로젝트 구조 설계

---
+++ react-netflix-prac/src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import netflix_logo from '../assets/img/netflix_logo.svg';
+import react_logo from '../assets/img/react_logo.png';
+
+const Nav = () => {
+    return (
+        <nav>
+            <img
+                alt="Netflix logo"
+                src={netflix_logo}
+                className="nav__logo"
+            />
+            <img alt="User logged" src={react_logo} className="nav__avatar" />
+        </nav>
+    );
+};
+
+export default Nav;
