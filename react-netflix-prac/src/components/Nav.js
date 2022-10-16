7358554d5e83adc9eff7756874dc04b2eab6955f
jaehan.kim <mju6013@naver.com>
2022-10-16 16:48:31 +0900

    Nav 구현

--- react-netflix-prac/src/components/Nav.js
+++ react-netflix-prac/src/components/Nav.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import netflix_logo from '../assets/img/netflix_logo.svg';
 import react_logo from '../assets/img/react_logo.png';
+import './Nav.css';
 
 const Nav = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener('scroll', () => {});
+    };
+  }, []);
+
   return (
-    <nav>
-      <img alt="Netflix logo" src={netflix_logo} className="nav__logo" />
+    <nav className={`nav ${show && 'nav__black'}`}>
+      <img
+        alt="Netflix logo"
+        src={netflix_logo}
+        className="nav__logo"
+        onClick={() => window.location.reload()}
+      />
       <img alt="User logged" src={react_logo} className="nav__avatar" />
     </nav>
   );
